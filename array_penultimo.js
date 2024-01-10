47226439dc71b57658fce6a4641020f792993d3b
Alexis Ojeda <145948164+alexisojeda12@users.noreply.github.com>
2024-01-09 22:36:07 -0600

    Create array_penultimo.js

---
+++ array_penultimo.js
@@ -0,0 +1,23 @@
+x = prompt("De que dimension es x?:  "); //3
+y = prompt("De que dimension es y?:  "); //3
+
+let completo = new Array(y);
+
+t=1;
+num=100;
+
+for (let i=0  ; i < y; i++){
+
+let base = new Array(x);
+  for (let j=0 ; j < x; j++){
+    base[j] = num;
+    num=num+101;
+}
+
+num=num-400;
+      completo[i]=base;
+      t++;
+}
+ for (let l=0 ; l < y; l++){
+    console.log(completo[l]);
+}
