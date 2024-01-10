c095711a4a402d9e80f00cfc6df2342d4e2a0ee7
Alexis Ojeda <145948164+alexisojeda12@users.noreply.github.com>
2024-01-09 22:27:52 -0600

    Create array_restando.js

---
+++ array_restando.js
@@ -0,0 +1,21 @@
+x = prompt("De que dimension es x?:  "); //3
+y = prompt("De que dimension es y?:  "); //3
+
+let completo = new Array(y);
+
+t=1;
+num=11.5;
+
+for (let i=0  ; i < y; i++){
+    
+let base = new Array(x);
+  for (let j=0 ; j < x; j++){
+    base[j] = num;
+    num--;
+}
+      completo[i]=base;
+      t++;
+}
+ for (let l=0 ; l < y; l++){
+    console.log(completo[l]);
+}
